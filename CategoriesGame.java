2d34cdd17a262289405dac87930698b8a62e7dae
Julia Jeng <juliajeng48@gmail.com>
2026-03-15 16:00:46 -0600

    Added method to play game, two logic errors

--- CategoriesGame.java
+++ CategoriesGame.java
@@ -12,7 +12,11 @@ public class CategoriesGame {
     private static List<String> categoryNames;
     public static void main(String[] args) {
         initCategories();
-        System.out.println(Arrays.toString(chooseCategories())); //test
+        String[] categories = chooseCategories();
+        //get word lists from chosen categories
+        List<String> cat1 = allCategories.get(categories[0]);
+        List<String> cat2 = allCategories.get(categories[1]);
+        playGame(5, 2, cat1, cat2); 
     }
 
     /** Sets up categories by reading from files */
@@ -34,12 +38,11 @@ public class CategoriesGame {
      */
     public static List<String> readFiles(String filename) {
         List<String> list = new ArrayList<>();
-        try {
-            Scanner fileScanner = new Scanner(new File(filename));
+        //try with resources, closes automatically after try block
+        try (Scanner fileScanner = new Scanner(new File(filename))) {
             while (fileScanner.hasNextLine()) {
                 list.add(fileScanner.nextLine());
             }
-            fileScanner.close();
             return list;
         }
         catch (FileNotFoundException e) {
@@ -72,5 +75,57 @@ public class CategoriesGame {
         chosenCategories[1] = categoriesToChoose.remove(randomIdx2);
 
         return chosenCategories;
+    }
+
+    /**Picks two categories and prints game to console
+     * @param number of words to give user
+     * @param chances, the number of tries the user has
+     * @param firstCat list of words in first category
+     * @param secCat list of words in second category
+     */
+    public static void playGame(int numWords, int chances, List<String> firstCat, List<String> secCat) {
+        //makes sure words are picked randomly
+        Collections.shuffle(firstCat);
+        Collections.shuffle(secCat);
+
+        //scanner to read user's answer
+        Scanner input = new Scanner(System.in);
+
+        //actual number of words to print (prevent index out of bounds)
+        int words = Math.min(firstCat.size(), numWords);
+
+        //pick the index of the word that doesn't fit in
+        int imposterIdx = (int)(Math.random()*words);
+
+        //Pick words 
+        /** ERROR skips like 2 indexes?? */
+        for (int i = 0; i < words; i++) {
+            if (i != imposterIdx) {
+                System.out.println(i+1 + ": " + firstCat.get(i));
+            }
+            else {
+                //get(0) to avoid index out of bounds if secCat.size() < firstCat.size()
+                System.out.println(i+1 + ": " + secCat.get(0));
+            }
+        }
+        boolean correct = false;
+        int tries = 0;
+        while (!correct && tries < chances) {
+            /** ERROR infinite loop */
+            while (!input.hasNextInt()) { 
+                //input.next(); //should clear?
+                System.out.print("Which word doesn't fit in (Type the number): ");
+                //int ans = input.nextInt();
+            }
+            int ans = input.nextInt();
+            if (ans == imposterIdx) {
+                System.out.println("You guessed correctly!");
+                correct = true;
+            }
+            else {
+                tries++;
+                System.out.println("Incorrect guess. You have " + (chances - tries) + " tries left.");
+            }
+        }
     }
 }
