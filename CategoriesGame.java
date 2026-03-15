722714294c95fd357a26fd0e0ac58cf54c901d9d
Julia Jeng <juliajeng48@gmail.com>
2026-03-15 10:40:05 -0600

    Added method to read textfiles and return an arraylist of words

--- CategoriesGame.java
+++ CategoriesGame.java
@@ -9,18 +9,32 @@ import java.util.*;
 public class CategoriesGame {
     private static Map<String, List<String>> allCategories;
     public static void main(String[] args) {
-        
+        initCategories();
     }
 
     /** Sets up categories by reading from file */
     public static void initCategories() {
         allCategories = new HashMap<>();
+        List<String> petWords = readFiles("pets.txt");
+    }
+
+    /** Reads file into an ArrayList
+     * @param name of text file
+     * @return ArrayList of words from file
+     */
+    public static List<String> readFiles(String filename) {
+        List<String> list = new ArrayList<>();
         try {
-            Scanner petp = new Scanner(new File("pets.txt")); //pet parser
-            
+            Scanner petp = new Scanner(new File(filename)); //pet parser
+            while (petp.hasNextLine()) {
+                list.add(petp.nextLine());
+            }
         }
         catch (FileNotFoundException e) {
             System.out.println("File was not found.");
+        }
+        finally {
+            return list;
         }
     }
 }
