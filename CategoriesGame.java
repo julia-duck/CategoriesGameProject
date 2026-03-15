6aef396d24b9d13a760bda2a1dc77a109e3b4c0b
Julia Jeng <juliajeng48@gmail.com>
2026-03-15 10:08:15 -0600

    Added method to initialize categories

--- CategoriesGame.java
+++ CategoriesGame.java
@@ -1,5 +1,21 @@
+/** Word categories game */
+import java.io.*;
+import java.util.*;
 public class CategoriesGame {
+    private static Map<String, List<String>> allCategories;
     public static void main(String[] args) {
         
+    }
+
+    /** Sets up categories by reading from file */
+    public static void initCategories() {
+        allCategories = new HashMap<>();
+        try {
+            Scanner petp = new Scanner(new File("pets.txt")); //pet parser
+            
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File was not found.");
+        }
     }
 }
